af7a39f62af0603a98fc147a15a25887f994da5d
ustair <nbhd404@gmail.com>
2022-01-17 23:36:57 +0900

    wip

--- trpl/guessing_game/src/main.rs
+++ trpl/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -6,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number); //秘密の数字は次の通り: {}
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessd: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("You win!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
