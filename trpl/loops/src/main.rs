13e2689479623b92abb873c4d4c60ed9fb414521
ustair <nbhd404@gmail.com>
2022-01-23 20:34:16 +0900

    wip

---
+++ trpl/loops/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!");
+
+    for x in 0..10 {
+        println!("{}", x);
+    }
+}
